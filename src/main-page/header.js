9050e7428cc11fcf4d87a02f14be8a5204d3a0a9
Rab Horner <robert.horner@tpicap.com>
2020-02-20 13:00:50 +0000

    Start on the header and logo import

---
+++ src/main-page/header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './GloboLogo.png'
+
+const Header = () => (
+    <header className="row">
+        <div className="col-md-5">
+            <img src={logo} className="logo" alt="logo" />
+        </div>
+        <div className="col-md-7 mt-5 subtitle">
+            Bringing new Homes right to your door
+        </div>
+        </header>
+);
+
