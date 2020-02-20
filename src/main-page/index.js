50acf09023ccedc4f0f543cf0c47f8498666fe82
Rab Horner <robert.horner@tpicap.com>
2020-02-20 14:42:11 +0000

    Refactoring to class syntax

--- src/main-page/index.js
+++ src/main-page/index.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './main-page.css';
 import Header from './header';
 
-function App() {
-  return (
-    <div className="container">
-      <Header/>
-    </div>
-  );
+class App extends Component {
+
+  render(){
+    return (
+      <div className="container">
+        <Header subtitle="Bringing New Homes Right To Your Door"/>
+      </div>
+    );
+  }
 }
 
 export default App;
